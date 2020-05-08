5be1cee3d424fa7a834692a24b61d237ad7933d9
Carolina Vero <carolina@w42.com.br>
2020-05-08 15:29:20 -0300

    scripts

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const url = 'https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72';
 const listRooms = document.querySelector('.rooms-block');
+let totalBlockEl = document.querySelector('.total-rooms');
 let totalValueEl = document.querySelector('.total-rooms__value');
 let countRooms = 0;
 let allRooms = [];
@@ -22,36 +23,36 @@ const showMapButton = document.querySelector(".show-map");
 // 25 places
 var coordinates = [
 
-    //Montes Claros
-    [-16.7173189, -43.8674957 ],
-    [-16.7176271, -43.883943 ], 
-    [-16.7081947, -43.8856271 ],
-    [-16.7103526, -43.8781599],
-    [-16.7122845, -43.8744906 ],
-    [-16.7107653, -43.8654821 ],
-    [-16.6997151, -43.8679461],
-    [-16.6997151, -43.8679461],
+    //Montes Claros: lat: -16.737, lng: -43.8647
+    [-16.7173189, -43.8674957, 'Montes Claros' ],
+    [-16.7176271, -43.883943, 'Montes Claros' ], 
+    [-16.7081947, -43.8856271, 'Montes Claros' ],
+    [-16.7103526, -43.8781599, 'Montes Claros'],
+    [-16.7122845, -43.8744906 , 'Montes Claros'],
+    [-16.7107653, -43.8654821, 'Montes Claros' ],
+    [-16.6997151, -43.8679461, 'Montes Claros'],
+    [-16.6997151, -43.8679461, 'Montes Claros'],
 
-    // BH
-    [-19.8909193, -43.9577007 ],
-    [-19.9158165, -43.9518642],
-    [-19.9150448, -43.9318603],
-    [-19.914722, -43.9293337],
-    [-19.9060568, -43.9418274],
-    [-19.8905234, -43.9291942],
-    [-19.9008987, -43.9291164],
-    [-19.885079, -43.9767414],  
-    [-19.8767382, -43.9887544],
-    [-19.8731841, -43.9775964],
-    [-19.8715949, -43.9802679],
-    [-19.868236, -43.9813423],
+    // BH lat: -19.8157, lng: -43.9542
+    [-19.8909193, -43.9577007 , 'Belo Horizonte'],
+    [-19.9158165, -43.9518642, 'Belo Horizonte'],
+    [-19.9150448, -43.9318603, 'Belo Horizonte'],
+    [-19.914722, -43.9293337, 'Belo Horizonte'],
+    [-19.9060568, -43.9418274, 'Belo Horizonte'],
+    [-19.8905234, -43.9291942, 'Belo Horizonte'],
+    [-19.9008987, -43.9291164, 'Belo Horizonte'],
+    [-19.885079, -43.9767414, 'Belo Horizonte'],  
+    [-19.8767382, -43.9887544, 'Belo Horizonte'],
+    [-19.8731841, -43.9775964, 'Belo Horizonte'],
+    [-19.8715949, -43.9802679, 'Belo Horizonte'],
+    [-19.868236, -43.9813423, 'Belo Horizonte'],
 
-    //SP
-    [-23.5815103, -46.6389331],
-    [-23.578521, -46.5560207],
-    [-23.5661307, -46.5152082],
-    [-23.5492347, -46.5339622],
-    [-23.5464415, -46.5431461] 
+    //SP lat: -23.533773, lng: - 46.625290
+    [-23.5815103, -46.6389331, "São Paulo"],
+    [-23.578521, -46.5560207, "São Paulo"],
+    [-23.5661307, -46.5152082, "São Paulo"],
+    [-23.5492347, -46.5339622, "São Paulo"],
+    [-23.5464415, -46.5431461, "São Paulo"] 
 
 ];
 
@@ -133,14 +134,46 @@ function setMarkers(map, rooms2) {
 var totalPrice = document.querySelector('.room__total__price');
 
 
+// Filter by Place 
+function filterByPlace(localValue, totalPriceValue) {
+    console.log("dentro do filtro", localValue);
+    console.log(allRooms)
+
+    var filtered = allRooms.filter(room => room.place == localValue);
+    totalValueEl = filtered.length;
+    console.log("filtered", filtered, totalValueEl);
+    renderRooms(filtered, totalPriceValue);
+
+}
+
 function calculateDays(e) {
     e.preventDefault();
+    console.log('calcular')
+    var localValue = localEl.value;
 
-    totalOfDays = Math.floor((Date.parse(checkoutEl.value) - Date.parse(checkinEl.value)) / 86400000); 
-    var totalPriceValue = totalOfDays * guests.value;
+    if (localValue == 0 || checkoutEl.value == 0 || checkoutEl.value == 0 || guests.value == 0) {
+        console.log("local vazio");
+        alert("Preencha todos os campos da busca")
 
-    //console.log('values typed ', totalOfDays, localEl.value, guests.value, totalPriceValue )
-    return totalPriceValue;
+    } else {
+        console.log(localValue)
+
+        totalOfDays = Math.floor((Date.parse(checkoutEl.value) - Date.parse(checkinEl.value)) / 86400000);
+        var totalPriceValue = totalOfDays * guests.value;
+
+        filterByPlace(localValue, totalPriceValue);
+
+        if (totalValueEl > 0) {
+            totalBlockEl.textContent = `Total de ${totalValueEl} acomodações em ${localValue}`;
+        } else {
+            totalBlockEl.innerHTML = `Nenhuma acomodação encontrada em ${localValue}`;
+        }
+
+    }
+
+
+
+
 
     // calcular total pra cada room
 }
@@ -159,6 +192,9 @@ function selectCurrentPage(page) {
 
     let link = document.getElementById(`page-${page}`)
     link.classList.add('active');
+
+    rooms2 = allRooms.slice((page - 1) * itemsForPage, itemsForPage * page);
+    renderRooms(rooms2);
 }
 
 // Loading
@@ -167,36 +203,44 @@ function stopLoading() {
 }
 
 // List all Rooms
-function renderRooms(page) {
+function renderRooms(rooms2, totalPriceValue = 0) {
 
-    selectCurrentPage(page);
-    
-    rooms2 = allRooms.slice((page - 1) * itemsForPage, itemsForPage * page);
-    //console.log("rooms", rooms2);
-    initMap(rooms2);
+    if(rooms2.length == 0) {
+        console.log("No room to render Map");
+    } else {
+        initMap(rooms2);
+    }
 
     const items = rooms2.reduce(
 
-        (html, room, index) => html +
-            `
-            <div class="room">
-                <div class="room__image-block">
-                    <a href="javascript:void(0)">
-                        <img class="room__image" src="${room.photo}" />
-                    </a>
-                </div>
-                <div class="room__type"> ${room.property_type} </div>
-                <div class="room__name"> 
-                    <a href="javascript:void(0)">
-                    ${room.name} 
-                    </a>
-                </div>
-                <div class="room__price"> R$${room.price},00 <span>/noite<span> </div>
-                <div class="room__price__total"> Total de R$,00 </div>
+        (html, room, index) => { 
             
-            </div>
-            `, ''
-    )
+            var total = '';
+
+            if(totalPriceValue > 0) {
+                total = `<div class="room__price__total"> Total de R${room.price * totalPriceValue},00 </div> `;
+            }
+
+            html +
+                `
+                <div class="room">
+                    <div class="room__image-block">
+                        <a href="javascript:void(0)">
+                            <img class="room__image" src="${room.photo}" />
+                        </a>
+                    </div>
+                    <div class="room__type"> ${room.property_type} </div>
+                    <div class="room__name"> 
+                        <a href="javascript:void(0)">
+                        ${room.name} 
+                        </a>
+                    </div>
+                    <div class="room__price"> R$${room.price},00 <span>/noite<span> </div>
+                    ${total}
+                
+                </div>
+                `, ''
+        })
     
     listRooms.innerHTML = '';
     listRooms.insertAdjacentHTML('beforeend', items);  
@@ -219,11 +263,12 @@ fetch(url)
             if(coordinates[index]) {
                 room.lat = coordinates[index][0];
                 room.lng = coordinates[index][1];
+                room.place = coordinates[index][2];
             }
             return room;
         })
 
-        renderRooms(1);
+        selectCurrentPage(1);
         stopLoading();
  
     })
